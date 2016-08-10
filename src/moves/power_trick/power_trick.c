bd7c13d5f72205e06ab4696b19ecc942ae4a17f7
SBird1337 <auerphilipp38@yahoo.de>
2016-08-10 22:53:50 +0200

    added power trick

---
+++ src/moves/power_trick/power_trick.c
@@ -0,0 +1,13 @@
+#include <battle.h>
+#include <bpre.h>
+
+u8 power_trick_swap() 
+{
+    custom_battle_elements.ptr->bank_affecting[battle_active_bank].powertrick = 1;
+    u16 *atk = &battle_participants[battle_active_bank].atk;
+    u16 *def = &battle_participants[battle_active_bank].def;
+    u16 switch_var = *atk;
+    *atk = *def;
+    *def = switch_var;
+    return 0;
+}
