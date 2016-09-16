5b45cb9e5028c12b5c30d851f59d60ba8eef1e7f
SBird1337 <auerphilipp38@yahoo.de>
2016-09-16 02:07:21 +0200

    added close combat

---
+++ src/moves/close_combat/close_combat.c
@@ -0,0 +1,27 @@
+#include <battle.h>
+
+extern void* bs_cc_lower_def;
+extern void* bs_cc_lower_sdef;
+extern void* bs_cc_lower_both;
+
+u8 close_combat_choose_script() {
+    if (battle_participants[battle_attacker_bank].def_buff == 0) {
+        if (battle_participants[battle_attacker_bank].sp_def_buff == 0)
+        {
+            battlescript_cursor = (void*) (0x081D694E);
+            return true;
+        }
+        else
+        {
+            battlescript_cursor = bs_cc_lower_sdef;
+            return true;
+        }
+    } else if (battle_participants[battle_attacker_bank].sp_def_buff == 0) {
+        battlescript_cursor = bs_cc_lower_def;
+        return true;
+    } else {
+        battlescript_cursor = bs_cc_lower_both;
+        return true;
+    }
+    return false;
+}
