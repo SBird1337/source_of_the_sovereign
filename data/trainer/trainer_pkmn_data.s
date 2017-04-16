7adf692b41c5a5a329370bb69f6b9b818a580f79
Philipp <Philipp@DESKTOP-FQ5SJ43>
2017-04-16 14:03:47 +0200

    trainer structs and trainers pokemon struct

--- data/trainer/trainer_pkmn_data.s
+++ data/trainer/trainer_pkmn_data.s
@@ -2,10 +2,388 @@
 .text
 .thumb
 
+.align 2
 .global trainer_pokemon_data
 trainer_pokemon_data:
 
+.align 2
 .global pokemon_von_id_1
 pokemon_von_id_1:
-.hword 0, 5, 4, 13, 33, 45, 0, 0, 0xFFFF
+.hword 0, 5, 895, 0
 
+.align 2
+.global pokemon_von_id_2
+pokemon_von_id_2:
+.hword 20, 6, 895, 0
+
+.align 2
+.global pokemon_von_id_3
+pokemon_von_id_3:
+.hword 50, 6, 895, 139
+
+.align 2
+.global pokemon_von_id_4
+pokemon_von_id_4:
+.hword 0, 5, 892, 0
+
+.align 2
+.global pokemon_von_id_5
+pokemon_von_id_5:
+.hword 20, 6, 892, 0
+
+.align 2
+.global pokemon_von_id_6
+pokemon_von_id_6:
+.hword 50, 6, 892, 139
+
+.align 2
+.global pokemon_von_id_7
+pokemon_von_id_7:
+.hword 0, 5, 895, 0
+
+.align 2
+.global pokemon_von_id_8
+pokemon_von_id_8:
+.hword 20, 6, 895, 0
+
+.align 2
+.global pokemon_von_id_9
+pokemon_von_id_9:
+.hword 50, 6, 895, 139
+
+.align 2
+.global pokemon_von_id_10
+pokemon_von_id_10:
+.hword 0, 5, 892, 0
+
+.align 2
+.global pokemon_von_id_11
+pokemon_von_id_11:
+.hword 20, 6, 892, 0
+
+.align 2
+.global pokemon_von_id_12
+pokemon_von_id_12:
+.hword 50, 6, 892, 139
+
+.align 2
+.global pokemon_von_id_13
+pokemon_von_id_13:
+.hword 0, 5, 789, 0
+
+.align 2
+.global pokemon_von_id_14
+pokemon_von_id_14:
+.hword 20, 5, 789, 0
+.hword 20, 6, 787, 0
+
+.align 2
+.global pokemon_von_id_15
+pokemon_von_id_15:
+.hword 50, 5, 789, 0
+.hword 50, 6, 787, 0
+
+.align 2
+.global pokemon_von_id_16
+pokemon_von_id_16:
+.hword 0, 5, 13, 0
+.hword 0, 5, 10, 0
+
+.align 2
+.global pokemon_von_id_17
+pokemon_von_id_17:
+.hword 20, 5, 13, 0
+.hword 20, 5, 10, 0
+.hword 20, 6, 290, 0
+
+.align 2
+.global pokemon_von_id_18
+pokemon_von_id_18:
+.hword 50, 5, 13, 0
+.hword 50, 5, 10, 0
+.hword 50, 6, 290, 0
+
+.align 2
+.global pokemon_von_id_19
+pokemon_von_id_19:
+.hword 0, 5, 46, 0
+.hword 0, 6, 795, 0
+.hword 0, 6, 291, 0
+
+.align 2
+.global pokemon_von_id_20
+pokemon_von_id_20:
+.hword 20, 5, 46, 0
+.hword 20, 6, 795, 0
+.hword 20, 6, 291, 0
+
+.align 2
+.global pokemon_von_id_21
+pokemon_von_id_21:
+.hword 50, 5, 46, 0
+.hword 50, 6, 795, 0
+.hword 50, 6, 291, 0
+.hword 50, 6, 293, 0
+
+.align 2
+.global pokemon_von_id_22
+pokemon_von_id_22:
+.hword 0, 5, 84, 0
+.hword 0, 6, 456, 0
+
+.align 2
+.global pokemon_von_id_23
+pokemon_von_id_23:
+.hword 20, 5, 84, 0
+.hword 20, 6, 456, 0
+
+.align 2
+.global pokemon_von_id_24
+pokemon_von_id_24:
+.hword 50, 5, 84, 0
+.hword 50, 6, 456, 0
+.hword 50, 6, 564, 0
+
+.align 2
+.global pokemon_von_id_25
+pokemon_von_id_25:
+.hword 0, 6, 566, 0
+
+.align 2
+.global pokemon_von_id_26
+pokemon_von_id_26:
+.hword 20, 6, 566, 0
+
+.align 2
+.global pokemon_von_id_27
+pokemon_von_id_27:
+.hword 50, 6, 566, 0
+.hword 50, 6, 568, 0
+
+.align 2
+.global pokemon_von_id_28
+pokemon_von_id_28:
+.hword 0, 6, 301, 0
+.hword 0, 7, 11, 0
+
+.align 2
+.global pokemon_von_id_29
+pokemon_von_id_29:
+.hword 20, 6, 301, 10, 106, 71, 0
+.hword 20, 7, 11, 10, 81, 106, 0
+
+.align 2
+.global pokemon_von_id_30
+pokemon_von_id_30:
+.hword 50, 6, 301, 10, 106, 71, 0
+.hword 50, 7, 11, 10, 81, 106, 0
+
+.align 2
+.global pokemon_von_id_31
+pokemon_von_id_31:
+.hword 0, 5, 165, 0
+.hword 0, 6, 14, 0
+.hword 0, 6, 11, 0
+.hword 0, 6, 293, 0
+
+.align 2
+.global pokemon_von_id_32
+pokemon_von_id_32:
+.hword 20, 5, 165, 0
+.hword 20, 6, 14, 0
+.hword 20, 6, 11, 0
+.hword 20, 6, 293, 0
+
+.align 2
+.global pokemon_von_id_33
+pokemon_von_id_33:
+.hword 50, 5, 165, 0
+.hword 50, 6, 14, 0
+.hword 50, 6, 11, 0
+.hword 50, 6, 293, 0
+.hword 50, 6, 291, 0
+
+.align 2
+.global pokemon_von_id_34
+pokemon_von_id_34:
+.hword 0, 6, 109, 0
+.hword 0, 7, 506, 0
+
+.align 2
+.global pokemon_von_id_35
+pokemon_von_id_35:
+.hword 20, 6, 109, 0
+.hword 20, 7, 506, 0
+
+.align 2
+.global pokemon_von_id_36
+pokemon_von_id_36:
+.hword 50, 6, 109, 0
+.hword 50, 7, 506, 0
+
+.align 2
+.global pokemon_von_id_37
+pokemon_von_id_37:
+.hword 0, 7, 784, 0
+.hword 0, 7, 797, 0
+
+.align 2
+.global pokemon_von_id_38
+pokemon_von_id_38:
+.hword 20, 7, 784, 0
+.hword 20, 7, 797, 0
+
+.align 2
+.global pokemon_von_id_39
+pokemon_von_id_39:
+.hword 50, 7, 784, 0
+.hword 50, 7, 797, 0
+
+.align 2
+.global pokemon_von_id_40
+pokemon_von_id_40:
+.hword 0, 7, 459, 0
+.hword 0, 8, 306, 0
+.hword 0, 8, 452, 0
+
+.align 2
+.global pokemon_von_id_41
+pokemon_von_id_41:
+.hword 20, 7, 459, 0
+.hword 20, 8, 306, 0
+.hword 20, 8, 452, 0
+
+.align 2
+.global pokemon_von_id_42
+pokemon_von_id_42:
+.hword 50, 7, 459, 0
+.hword 50, 8, 306, 0
+.hword 50, 8, 452, 0
+
+.align 2
+.global pokemon_von_id_43
+pokemon_von_id_43:
+.hword 0, 9, 291, 0
+
+.align 2
+.global pokemon_von_id_44
+pokemon_von_id_44:
+.hword 20, 9, 291, 0
+
+.align 2
+.global pokemon_von_id_45
+pokemon_von_id_45:
+.hword 50, 10, 292, 0
+
+.align 2
+.global pokemon_von_id_46
+pokemon_von_id_46:
+.hword 0, 9, 167, 0
+.hword 0, 9, 386, 0
+
+.align 2
+.global pokemon_von_id_47
+pokemon_von_id_47:
+.hword 20, 9, 167, 0
+.hword 20, 9, 386, 0
+
+.align 2
+.global pokemon_von_id_48
+pokemon_von_id_48:
+.hword 50, 9, 167, 0
+.hword 50, 10, 386, 0
+
+.align 2
+.global pokemon_von_id_49
+pokemon_von_id_49:
+.hword 0, 9, 48, 0
+.hword 0, 10, 717, 0
+
+.align 2
+.global pokemon_von_id_50
+pokemon_von_id_50:
+.hword 20, 9, 48, 0
+.hword 20, 10, 717, 0
+
+.align 2
+.global pokemon_von_id_51
+pokemon_von_id_51:
+.hword 50, 9, 48, 0
+.hword 50, 10, 717, 0
+
+.align 2
+.global pokemon_von_id_52
+pokemon_von_id_52:
+.hword 0, 10, 294, 0
+
+.align 2
+.global pokemon_von_id_53
+pokemon_von_id_53:
+.hword 20, 10, 294, 0
+
+.align 2
+.global pokemon_von_id_54
+pokemon_von_id_54:
+.hword 50, 10, 294, 0
+
+.align 2
+.global pokemon_von_id_55
+pokemon_von_id_55:
+.hword 0, 9, 79, 0
+.hword 0, 10, 804, 0
+
+.align 2
+.global pokemon_von_id_56
+pokemon_von_id_56:
+.hword 20, 9, 79, 0
+.hword 20, 10, 804, 0
+
+.align 2
+.global pokemon_von_id_57
+pokemon_von_id_57:
+.hword 50, 9, 79, 0
+.hword 50, 10, 804, 0
+
+.align 2
+.global pokemon_von_id_58
+pokemon_von_id_58:
+.hword 0, 9, 346, 0
+.hword 0, 10, 220, 0
+.hword 0, 10, 934, 0
+
+.align 2
+.global pokemon_von_id_59
+pokemon_von_id_59:
+.hword 30, 9, 346, 0
+.hword 30, 10, 220, 0
+.hword 30, 10, 934, 0
+
+.align 2
+.global pokemon_von_id_60
+pokemon_von_id_60:
+.hword 60, 9, 346, 0
+.hword 60, 10, 220, 0
+.hword 60, 10, 934, 0
+.hword 60, 11, 512, 0
+
+.align 2
+.global pokemon_von_id_61
+pokemon_von_id_61:
+.hword 0, 9, 341, 0
+.hword 0, 10, 635, 0
+.hword 0, 10, 936, 0
+
+.align 2
+.global pokemon_von_id_62
+pokemon_von_id_62:
+.hword 30, 9, 341, 0
+.hword 30, 10, 635, 0
+.hword 30, 10, 936, 0
+
+.align 2
+.global pokemon_von_id_63
+pokemon_von_id_63:
+.hword 60, 9, 341, 0
+.hword 60, 10, 635, 0
+.hword 60, 10, 936, 0
+.hword 60, 11, 215, 0
