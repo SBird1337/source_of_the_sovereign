8a01e252f398f7ba032e80f90544ee3d40448f80
SBird1337 <auerphilipp38@yahoo.de>
2017-04-18 17:26:38 +0200

    level up evolution

--- data/raw_defines.s
+++ data/raw_defines.s
@@ -6,6 +6,10 @@
 objects:
     .word 0x0202063C
 
+.global evolutions
+evolutions:
+    .word 0x08259754
+
 .global ts_pals
 ts_pals:
     .incbin "sots-private/assets/misc/title/pal.bin"
--- src/evolution/evolution_methods.c
+++ src/evolution/evolution_methods.c
@@ -39,6 +39,66 @@ enum evo_source
     STONE_REQUEST
 };
 
+struct evo_information
+{
+    u16 method;
+    u16 argument;
+    u16 evolve_to;
+    u8 arg1;
+    u8 arg2;
+};
+
+struct evo_result
+{
+    u8 can_evolve;
+    u8 consume_item;
+    u8 evolve_to;
+};
+
+#define EVO_NULL {0,0,0,0,0}
+#define EVO_LEVEL_UP 4
+#define MAX_EVOLUTIONS 3
+
+struct evo_information evolutions[][MAX_EVOLUTIONS] = {
+    {EVO_NULL,EVO_NULL,EVO_NULL},
+    {{EVO_LEVEL_UP,16,2,0,0},EVO_NULL,EVO_NULL},
+    {{EVO_LEVEL_UP,32,3,0,0},EVO_NULL,EVO_NULL},
+};
+
+struct evo_call_arguments
+{
+    u16 item;
+    u16 level;
+    enum evo_source source;
+    u16 stoneId;
+    struct pokemon* poke;
+    struct evo_information evolution;
+};
+
+typedef struct evo_result (*evolution_callback)(struct evo_call_arguments);
+
+struct evo_result evolve_by_level(struct evo_call_arguments arguments)
+{
+    if(arguments.source != LEVEL_UP)
+    {
+        return (struct evo_result){false, false, 0};
+    }
+    if(arguments.evolution.argument <= arguments.level)
+    {
+        return (struct evo_result){true, false, arguments.evolution.evolve_to};
+    }
+    return (struct evo_result){false, false, 0};
+}
+
+static evolution_callback evolution_methods[] =
+{
+    NULL,
+    NULL, //TODO: Happiness
+    NULL, //TODO: Happiness DAY
+    NULL, //TODO: Happiness NIGHT
+    evolve_by_level
+};
+
 u16 evolution_try_evolve(struct pokemon* pokemon, enum evo_source source, u16 stoneId)
 {
     u16 held_item = pokemon_get_attribute(pokemon, ATTR_HELD_ITEM, NULL);
@@ -47,6 +107,29 @@ u16 evolution_try_evolve(struct pokemon* pokemon, enum evo_source source, u16 st
     //TODO PID
     dprintf("Species %d tried to evolve.\n", species);
     dprintf("Cause: %d\n", source);
-    dprintf("Held Item: %d; Level: %d\nBlocking evolution for debug.", held_item, level);
+    struct evo_information* current_evolution_structure = evolutions[species];
+    struct evo_result result;
+    for(int i = 0; i < MAX_EVOLUTIONS; ++i)
+    {
+        if(current_evolution_structure[i].method != 0)
+        {
+            dprintf("found valid evolution with method %d for species %d", current_evolution_structure[i].method, species);
+            struct evo_call_arguments args = {held_item, level, source, stoneId, pokemon, current_evolution_structure[i]};
+            result = evolution_methods[current_evolution_structure[i].method](args);
+            if(result.can_evolve)
+                break;
+        }
+    }
+    if(result.can_evolve)
+    {
+        if(result.consume_item)
+        {
+            //TODO: Consume
+        }
+        return result.evolve_to;
+    }
+    else
+        return 0;
+    /*evolve everything to mewtu for jiggs & giggles*/
     return 150;
 }
