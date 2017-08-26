be40998bf4ad7557d2facd4fa0f355503625f1cd
SBird1337 <auerphilipp38@yahoo.de>
2017-08-27 00:29:19 +0200

    started new special effects

--- src/include/assets/mug/mug_08.h
+++ src/include/assets/mug/mug_08.h
@@ -8,7 +8,7 @@
 //	+ 16 tiles lz77 compressed
 //	Total size: 32 + 504 = 536
 //
-//	Time-stamp: 2017-05-08, 11:18:02
+//	Time-stamp: 2017-08-26, 17:30:27
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
---
+++ src/specials/special_effects/crystal_fade.c
@@ -0,0 +1,29 @@
+#include <pokeagb/pokeagb.h>
+
+void task_fade_flash(u8 id) {
+    switch (tasks[id].priv[1]) {
+    case 0:
+        fade_screen(0xFFFFFFFF, 5, 0, 16, tasks[id].priv[0]);
+        tasks[id].priv[1]++;
+        break;
+    case 1:
+        if (!pal_fade_control.active) {
+            fade_screen(0xFFFFFFFF, 5, 16, 0, tasks[id].priv[0]);
+            tasks[id].priv[1]++;
+        }
+        break;
+    case 2:
+        if(!pal_fade_control.active)
+        {
+            task_del(id);
+            scr
+        }
+        break;
+    }
+}
+
+void sp_crystal_fade(void) {
+    u16 color = var_8000;
+    u8 t_id = task_add(task_fade_flash);
+    tasks[t_id].priv[0] = color;
+}
