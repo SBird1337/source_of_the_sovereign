2e400f0cad4206707447229120d0cd685d8d15a9
SBird1337 <auerphilipp38@yahoo.de>
2018-01-06 01:04:21 +0100

    evolution_methods.h

--- src/evolution/evolution_methods.c
+++ src/evolution/evolution_methods.c
@@ -35,6 +35,7 @@
 #include <constants/moves.h>
 #include <pokemon.h>
 #include <constants/ptypes.h>
+#include <evolution_methods.h>
 
 #define EVO_NULL              \
     {                         \
@@ -46,37 +47,6 @@
 
 #define HAPPY_BOUND 219
 #define BEAUTY_BOUND 170
-
-#define GENDER_DC 0
-#define GENDER_MALE 1
-#define GENDER_FEMALE 2
-
-#define EVO_HAPPINESS 1
-#define EVO_LEVEL_UP 4
-#define EVO_TRADE 5
-#define EVO_TRADE_ITEM 6
-#define EVO_STONE 7
-#define EVO_ATK 8
-#define EVO_DEF 9
-#define EVO_ADEQU 10
-#define EVO_PERSO_HIGH 11
-#define EVO_PERSO_LOW 12
-#define EVO_SPAWN 13
-#define EVO_SPAWNED 14
-#define EVO_BEAUTY 15
-#define EVO_WEAR_ITEM 16
-#define EVO_WEAR_ITEM_NIGHT 17
-#define EVO_WEAR_ITEM_DAY 18
-#define EVO_LEVEL_NIGHT 19
-#define EVO_LEVEL_DAY 20
-#define EVO_LEVEL_VAR 21
-#define EVO_LEVEL_MOVE 22
-#define EVO_LEVEL_POKEMON 23
-#define EVO_LEVEL_TYPE 24
-#define EVO_LEVEL_MOVE_TYPE 25
-#define EVO_MEGA_ONE 26
-#define EVO_MEGA_TWO 27
-#define EVO_PROTO 28
 
 enum evo_source
 {
---
+++ src/include/evolution_methods.h
@@ -0,0 +1,35 @@
+#ifndef EVO_METHODS_H_
+#define EVO_METHODS_H_
+
+#define GENDER_DC 0
+#define GENDER_MALE 1
+#define GENDER_FEMALE 2
+
+#define EVO_HAPPINESS 1
+#define EVO_LEVEL_UP 4
+#define EVO_TRADE 5
+#define EVO_TRADE_ITEM 6
+#define EVO_STONE 7
+#define EVO_ATK 8
+#define EVO_DEF 9
+#define EVO_ADEQU 10
+#define EVO_PERSO_HIGH 11
+#define EVO_PERSO_LOW 12
+#define EVO_SPAWN 13
+#define EVO_SPAWNED 14
+#define EVO_BEAUTY 15
+#define EVO_WEAR_ITEM 16
+#define EVO_WEAR_ITEM_NIGHT 17
+#define EVO_WEAR_ITEM_DAY 18
+#define EVO_LEVEL_NIGHT 19
+#define EVO_LEVEL_DAY 20
+#define EVO_LEVEL_VAR 21
+#define EVO_LEVEL_MOVE 22
+#define EVO_LEVEL_POKEMON 23
+#define EVO_LEVEL_TYPE 24
+#define EVO_LEVEL_MOVE_TYPE 25
+#define EVO_MEGA_ONE 26
+#define EVO_MEGA_TWO 27
+#define EVO_PROTO 28
+
+#endif /*EVO_METHODS_H_*/
