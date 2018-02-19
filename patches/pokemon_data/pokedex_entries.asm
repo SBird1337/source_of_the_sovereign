13d85b9dcbc8e4de65618e37daaaf4a2e392169e
DragonflySotS <Dragonflysots@gmail.com>
2018-02-19 19:29:29 +0100

    Pokédex Fix

--- patches/pokemon_data/pokedex_entries.asm
+++ patches/pokemon_data/pokedex_entries.asm
@@ -1,23 +1,23 @@
 .org 0x08088e34
-.word pokedex_spezies_name1
+.word str_pokedex_spezies_1
 
 .org 0x08088e5c
-.word pokedex_spezies_name1
+.word str_pokedex_spezies_1
 
 .org 0x08088e70
-.word pokedex_spezies_name1
+.word str_pokedex_spezies_1
 
 .org 0x08105860
-.word pokedex_spezies_name1
+.word str_pokedex_spezies_1
 
 .org 0x08105974
-.word pokedex_spezies_name1
+.word str_pokedex_spezies_1
 
 .org 0x08105ACC
-.word pokedex_spezies_name1
+.word str_pokedex_spezies_1
 
 .org 0x08105D44
-.word pokedex_spezies_name1
+.word str_pokedex_spezies_1
 
 .org 0x081066C8
-.word pokedex_spezies_name1
+.word str_pokedex_spezies_1
