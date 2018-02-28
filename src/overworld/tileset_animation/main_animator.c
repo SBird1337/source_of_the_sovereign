127024238ddac6154ad37866e1ca52223cded84e
SBird1337 <auerphilipp38@yahoo.de>
2018-02-28 01:14:00 +0100

    added hesperia animations, resolves #89

--- src/overworld/tileset_animation/main_animator.c
+++ src/overworld/tileset_animation/main_animator.c
@@ -1,9 +1,14 @@
-#include <pokeagb/pokeagb.h>
 #include <agb_debug.h>
-#include <tileset_animation/smoke.h>
-#include <tileset_animation/water_stone.h>
+#include <pokeagb/pokeagb.h>
 #include <tileset_animation/flowers_mag.h>
 #include <tileset_animation/flowers_ora.h>
+#include <tileset_animation/smoke.h>
+#include <tileset_animation/water_deep.h>
+#include <tileset_animation/water_normal.h>
+#include <tileset_animation/water_sea.h>
+#include <tileset_animation/water_edge.h>
+#include <tileset_animation/water_stone.h>
+#include <tileset_animation/waterfall.h>
 
 #define NUM_MAX_ANIMATIONS 16
 
@@ -22,11 +27,18 @@ struct TilesetAnimation {
     const void *image;
 };
 
+#define AS_WATER 9
+
 const struct TilesetAnimation hesperia_second_animations[] = {
     {.tile_start = 0x10C, .frame_length = 14, .tile_length = 4, .frame_count = 10, .image = flowers_magTiles},
     {.tile_start = 0x110, .frame_length = 14, .tile_length = 4, .frame_count = 15, .image = flowers_oraTiles},
     {.tile_start = 0x114, .frame_length = 7, .tile_length = 4, .frame_count = 8, .image = smokeTiles},
     {.tile_start = 0x13C, .frame_length = 7, .tile_length = 4, .frame_count = 4, .image = water_stoneTiles},
+    {.tile_start = 0x118, .frame_length = 4, .tile_length = 36, .frame_count = 8, .image = waterfallTiles},
+    {.tile_start = 0x140, .frame_length = AS_WATER, .tile_length = 36, .frame_count = 8, .image = water_normalTiles},
+    {.tile_start = 0x168, .frame_length = AS_WATER, .tile_length = 4, .frame_count = 8, .image = water_seaTiles},
+    {.tile_start = 0x16C, .frame_length = AS_WATER, .tile_length = 4, .frame_count = 8, .image = water_deepTiles},
+    {.tile_start = 0x164, .frame_length = AS_WATER, .tile_length = 4, .frame_count = 8, .image = water_edgeTiles},
     {.tile_start = 0, .frame_length = 0, .tile_length = 0, .frame_count = 0, .image = (void *)0xFFFFFFFF},
 };
 
@@ -37,9 +49,9 @@ void animate_from_structure(const struct TilesetAnimation *anim, u16 tile_skip, 
         void *current_vram = vram_address + (0x20 * anim[cur_anim].tile_start);
 
         if (anim_state->cur_frame[cur_anim] == 0) {
-            memcpy(current_vram, anim[cur_anim].image +
-                    (0x20 * anim[cur_anim].tile_length * anim_state->cur_tile[cur_anim]),
-                    anim[cur_anim].tile_length * 0x20);
+            memcpy(current_vram,
+                   anim[cur_anim].image + (0x20 * anim[cur_anim].tile_length * anim_state->cur_tile[cur_anim]),
+                   anim[cur_anim].tile_length * 0x20);
         }
 
         anim_state->cur_frame[cur_anim] += 1;
@@ -55,7 +67,8 @@ void animate_from_structure(const struct TilesetAnimation *anim, u16 tile_skip, 
     }
     if (cur_anim >= NUM_MAX_ANIMATIONS) {
         dprintf("Warning! Animation State array not big enough to play all animations\n"
-                "Please increase the limit in %s\n", __FILE__);
+                "Please increase the limit in %s\n",
+                __FILE__);
     }
 }
 
@@ -76,7 +89,7 @@ void main_animator_init(void) {
 
 void main_second_animator_init(void) {
     blockset_two_current_frame = 0;
-    blockset_two_max_frame = 0x3C0;
+    blockset_two_max_frame = 0x3624;
     blockset_two_animator = main_second_animator;
 
     for (int i = 0; i < NUM_MAX_ANIMATIONS; i++) {
