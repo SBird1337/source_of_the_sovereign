7c2e4d07189b6535bc8e0839f87a77bc2cacba68
Michael Panzlaff <michael.panzlaff@fau.de>
2018-06-02 19:14:47 +0200

    cancel update script execution if something goes wrong

--- update_all.sh
+++ update_all.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -eu
+
 main_repo=$(pwd)
 
 git_save() {
@@ -9,6 +11,7 @@ git_save() {
         git stash push --include-untracked
         return 0
     else
+        set +e
         return 1
     fi
 }
@@ -27,6 +30,7 @@ echo "*** Updating Battle Engine repository ***"
 cd $main_repo/battle_engine
 git_save
 result=$?
+set -e
 git checkout master
 git pull
 git_restore $result
@@ -36,6 +40,7 @@ echo "*** Updating g3headers repository ***"
 cd $main_repo/g3headers
 git_save
 result=$?
+set -e
 git checkout master
 git pull
 git_restore $result
@@ -45,6 +50,7 @@ echo "*** Updating private repository ***"
 cd $main_repo/sots-private
 git_save
 result=$?
+set -e
 git checkout master
 git pull
 git_restore $result
