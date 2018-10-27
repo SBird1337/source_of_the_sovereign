85d95bdcc816a154d311e87a6f13ff3cddf6ef58
dragonflysots <dragonflysots@gmail.com>
2018-10-28 01:37:20 +0200

    Add Namespace & NPC Texte Hesperia Outdoor

--- data/string/map_names.s
+++ data/string/map_names.s
@@ -6,18 +6,19 @@
 map_name_table:
 .word str_map_name_hesperia
 .word str_map_name_carun
+.word str_map_name_urban
 .word str_map_name_hesperiapfad
 .word str_map_name_hesperiazweig
 .word str_map_name_carunpfad
-.word str_map_name_invalid
-.word str_map_name_invalid
-.word str_map_name_invalid
-.word str_map_name_invalid
-.word str_map_name_invalid
-.word str_map_name_invalid
-.word str_map_name_invalid
-.word str_map_name_invalid
-.word str_map_name_invalid
+.word str_map_name_yelniapfad
+.word str_map_name_wald_von_yelnia
+.word str_map_name_hesperiagraben
+.word str_map_name_reberatunnel
+.word str_map_name_baum_des_lebens
+.word str_map_name_rebera_gebirge_ost
+.word str_map_name_ruine_der_weachter
+.word str_map_name_trainerschule
+.word str_map_name_waldhain
 .word str_map_name_invalid
 .word str_map_name_invalid
 .word str_map_name_invalid
@@ -75,6 +76,10 @@ str_map_name_carun:
 .string LAN_DE "Carun City"
 .string LAN_EN "Carun City"
 
+str_map_name_urban:
+.string LAN_DE "Urbania City"
+.string LAN_EN "Urbania City"
+
 str_map_name_hesperiapfad:
 .string LAN_DE "Hesperiapfad"
 .string LAN_EN "Hesperia Trail"
@@ -86,6 +91,42 @@ str_map_name_hesperiazweig:
 str_map_name_carunpfad:
 .string LAN_DE "Carunpfad"
 .string LAN_EN "Carun Trail"
+
+str_map_name_yelniapfad:
+.string LAN_DE "Yelniapfad"
+.string LAN_EN "Yelnia Trail"
+
+str_map_name_wald_von_yelnia:
+.string LAN_DE "Wald von Yelnia"
+.string LAN_EN "Forest of Yelnia"
+
+str_map_name_hesperiagraben:
+.string LAN_DE "Hesperiagraben"
+.string LAN_EN "Hesperiagravaben"
+
+str_map_name_reberatunnel:
+.string LAN_DE "Reberatunnel"
+.string LAN_EN "Reberatunnel"
+
+str_map_name_baum_des_lebens:
+.string LAN_DE "Baum des Lebens"
+.string LAN_EN "Tree of Life"
+
+str_map_name_rebera_gebirge_ost:
+.string LAN_DE "Öst. Reb. Gebirge"
+.string LAN_EN "East. Reb. Mount."
+
+str_map_name_ruine_der_weachter:
+.string LAN_DE "Ruine der Wächter"
+.string LAN_EN "Ruin of Guardians"
+
+str_map_name_trainerschule:
+.string LAN_DE "Trainerschule"
+.string LAN_EN "Trainer School"
+
+str_map_name_waldhain:
+.string LAN_DE "Waldhain"
+.string LAN_EN "Woodland"
 
 str_map_name_invalid:
 .string LAN_DE "Invalider Name"
